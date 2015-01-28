5b017cef22dd0ab01a256f84130787e13cf6b205
Jamie Scott <JayRScott@gmail.com>
2015-01-28 18:57:12 +0000

    1.1 Release
    Renamed Gittip to Gratipay.
    Widget script updated.

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # WP Gittip #
 
-**Contributors:** daankortenbach  
-**Donate link:** https://www.gittip.com/daankortenbach/  
-**Tags:** Gittip, widget  
-**Requires at least:** 3.0.1  
-**Tested up to:** 3.6  
-**Stable tag:** 1.0  
-**License:** GNU General Public License v2.0 (or later)  
-**License URI:** http://www.opensource.org/licenses/gpl-license.php  
+**Contributors:** daankortenbach, KakersUK
+**Tags:** Gratipay, Gittip, widget
+**Requires at least:** 3.0.1
+**Tested up to:** 4.1
+**Stable tag:** 1.1
+**License:** GPL2
+**License URI:** http://www.opensource.org/licenses/gpl-license.php
 
 ## Description ##
 
-Need a simple way to show a Gittip widget on your site? WP Gittip allows you to display a Gittip widget on your site.
+Do you need a simple way to show a Gratipay widget on your site? WP Gratipay allows you to display a Gratipay widget on your site.
 
-It's super easy to use, just install, activate, drag the widget in a sidebar and enter your Gittip username.
+It's super easy to use, just install, activate, drag the widget in a sidebar and enter your Gratipay username.
 
 ## Installation ##
 
-1. Upload `WP Gittip` to the `/wp-content/plugins/` directory
+1. Upload the `wp-gratipay` folder to your `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Drag the WP Gittip widget in a sidebar
-1. Enter you Gittip username and optionally a title
+1. Drag and drop the WP Gratipay widget to your a sidebar
+1. Enter your Gratipay username and optionally a title
 1. Get tips!
 
 ## Changelog ##
+
+### 1.1 ###
+* Renamed Gittip to Gratipay.
+* Widget script updated.
 
 ### 1.0 ###
 * Initial version.
--- wp-gratipay/wp-gratipay.php
+++ wp-gratipay/wp-gratipay.php
@@ -1,33 +1,28 @@
 <?php
 /*
-	Plugin Name: WP Gittip
-	Plugin URI: http://daan.kortenba.ch/wp-gittip
+ *	Plugin Name: WP Gratipay
+ *	Plugin URI: https://github.com/KakersUK/WP-Gratipay
+ *	Description: WP Gratipay allows you to display a Gratipay widget on your site.
+ *	Version: 1.1
+ *	Author: Jamie Scott
+ *	Author URI: http://www.kakersuk.com/
+ *	License: GPL2
+ */
 
-	Description: WP Gittip allows you to display a Gittip widget on your site.
-
-	Author: Daan Kortenbach
-	Author URI: http://daan.kortenba.ch/
-
-	Version: 1.0
-
-	License: GNU General Public License v2.0 (or later)
-	License URI: http://www.opensource.org/licenses/gpl-license.php
-*/
-
-add_action( 'widgets_init', 'wp_gittip_load_widget' );
+add_action( 'widgets_init', 'wp_gratipay_load_widget' );
 /**
  * Register widget
  */
-function wp_gittip_load_widget() {
+function wp_gratipay_load_widget() {
 
-	register_widget( 'WP_Gittip_Widget' );
+	register_widget( 'WP_Gratipay_Widget' );
 
 }
 
 /**
- * WP Gittip widget class.
+ * WP Gratipay widget class.
  */
-class WP_Gittip_Widget extends WP_Widget {
+class WP_Gratipay_Widget extends WP_Widget {
 
 	/**
 	 * Holds widget settings defaults, populated in constructor.
@@ -43,21 +38,21 @@ class WP_Gittip_Widget extends WP_Widget {
 
 		$this->defaults = array(
 			'title'           => '',
-			'gittip_username' => '',
+			'gratipay_username' => '',
 		);
 
 		$widget_ops = array(
-			'classname'   => 'wp-gittip',
-			'description' => __( 'Displays a Gittip widget', 'wpgittip' ),
+			'classname'   => 'wp-gratipay',
+			'description' => __( 'Displays a Gratipay widget', 'wpgratipay' ),
 		);
 
 		$control_ops = array(
-			'id_base' => 'wp-gittip',
+			'id_base' => 'wp-gratipay',
 			'width'   => 200,
 			'height'  => 250,
 		);
 
-		parent::__construct( 'wp-gittip', __( 'WP Gittip', 'wpgittip' ), $widget_ops, $control_ops );
+		parent::__construct( 'wp-gratipay', __( 'WP Gratipay', 'wpgratipay' ), $widget_ops, $control_ops );
 
 	}
 
@@ -79,7 +74,7 @@ class WP_Gittip_Widget extends WP_Widget {
 			if ( ! empty( $instance['title'] ) )
 				echo $before_title . apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base ) . $after_title;
 
-			printf( '<script data-gittip-username="%s" src="https://www.gittip.com/assets/widgets/0002.js"></script>', $instance['gittip_username'] );
+			printf( '<script data-gratipay-username="%s" src="//grtp.co/v1.js"></script>', $instance['gratipay_username'] );
 
 		echo $after_widget;
 
@@ -99,7 +94,7 @@ class WP_Gittip_Widget extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 
 		$new_instance['title']           = strip_tags( $new_instance['title'] );
-		$new_instance['gittip_username'] = strip_tags( $new_instance['gittip_username'] );
+		$new_instance['gratipay_username'] = strip_tags( $new_instance['gratipay_username'] );
 
 		return $new_instance;
 
@@ -117,13 +112,13 @@ class WP_Gittip_Widget extends WP_Widget {
 
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'wpgittip' ); ?>:</label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'wpgratipay' ); ?>:</label>
 			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>" class="widefat" />
 		</p>
 
 		<p>
-			<label for="<?php echo $this->get_field_id( 'gittip_username' ); ?>"><?php _e( 'Gittip username', 'wpgittip' ); ?>:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'gittip_username' ); ?>" name="<?php echo $this->get_field_name( 'gittip_username' ); ?>" value="<?php echo esc_attr( $instance['gittip_username'] ); ?>" class="widefat" />
+			<label for="<?php echo $this->get_field_id( 'gratipay_username' ); ?>"><?php _e( 'Gratipay username', 'wpgratipay' ); ?>:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'gratipay_username' ); ?>" name="<?php echo $this->get_field_name( 'gratipay_username' ); ?>" value="<?php echo esc_attr( $instance['gratipay_username'] ); ?>" class="widefat" />
 		</p>
 
 		<?php
